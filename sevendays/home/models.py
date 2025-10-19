efe5c3a5d75ee84356c22a492af091c32c266255
Ontisitse Manyeneng <ontimanyeneng440@gmail.com>
2025-10-19 14:49:21 +0200

    Banner title

--- sevendays/home/models.py
+++ sevendays/home/models.py
@@ -4,6 +4,6 @@ from wagtail.models import Page
 
 
 class HomePage(Page):
-    banner_title=models.CharField(max_length=140,default="Welcome to my home page")
+    banner_title=models.CharField(max_length=140,default="Welcome to my home wagtail")
 
-    
+
