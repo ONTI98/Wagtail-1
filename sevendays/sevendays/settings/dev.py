02c933fd9502b5f2b99e8e3e61a8d0836c6c7e92
Ontisitse Manyeneng <ontimanyeneng440@gmail.com>
2025-10-19 13:46:06 +0200

    Wagtail initial set up

---
+++ sevendays/sevendays/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-t*t4jbb)ax=e4_*ewcwq#kujy(s6-lz4(+oa3)o)&)+4kuk1j7"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
