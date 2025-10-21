4c82073df3af3c707c3a044ed63f53c7392d7664
Ontisitse Manyeneng <ontimanyeneng440@gmail.com>
2025-10-21 15:18:49 +0200

    add author model and variable/field

--- sevendays/generic/models.py
+++ sevendays/generic/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 from wagtail.models import Page
 from wagtail.admin.panels import FieldPanel
-
+from wagtail.snippets.models import register_snippet
 
 class GenericPage(Page):
     banner_title=models.CharField(max_length=140,default="Welcome to my generic page")
@@ -15,7 +15,13 @@ class GenericPage(Page):
         on_delete=models.SET_NULL,
         related_name='+',   
     )
-
+    author=models.ForeignKey(
+        'Author' , #this will point to the author class below, for that we use a string
+         null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name='+', 
+    )
     #extends content panels to include banner title
     content_panels=Page.content_panels+[
         FieldPanel("banner_title"),
@@ -23,3 +29,30 @@ class GenericPage(Page):
         FieldPanel("banner_image"),
 
     ]
+
+@register_snippet
+class Author(models.Model):
+    name=models.CharField(max_length=140,blank=True,)
+    title=models.CharField(max_length=140,blank=True,)
+    company_name=models.CharField(max_length=140,blank=True,)
+    company_url=models.URLField(blank=True)
+    image=models.ForeignKey(
+        'wagtailimages.Image',
+        on_delete=models.SET_NULL,
+        blank=False,
+        null=True,
+        related_name="+",
+    )
+
+   
+
+    panels=[
+        FieldPanel("name"),
+        FieldPanel("title"),
+        FieldPanel("company_name"),
+        FieldPanel("company_url"),
+        FieldPanel("image")
+    ] 
+    
+    def __str__(self):
+        return self.name
