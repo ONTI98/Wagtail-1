05a59c5a945c2b9c53c8b8b26b7326b47c18402b
Ontisitse Manyeneng <ontimanyeneng440@gmail.com>
2025-10-21 13:53:05 +0200

    add image to page

--- sevendays/generic/models.py
+++ sevendays/generic/models.py
@@ -7,9 +7,19 @@ from wagtail.admin.panels import FieldPanel
 
 class GenericPage(Page):
     banner_title=models.CharField(max_length=140,default="Welcome to my generic page")
+    introduction=models.TextField(max_length=140)
+    banner_image=models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name='+',   
+    )
 
     #extends content panels to include banner title
     content_panels=Page.content_panels+[
         FieldPanel("banner_title"),
+        FieldPanel("introduction"),
+        FieldPanel("banner_image"),
 
     ]
